432b8514f4941a3521828785110d91d7fb47a4aa
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-01-11 08:33:42 -0300

    estrutura Set

---
+++ ESNext/Set.js
@@ -0,0 +1,12 @@
+// Set é uma estrutura de dados em JavaScript que armazena valores únicos, o que significa que não pode haver elementos duplicados no conjunto.
+const times = new Set()
+
+times.add('Flamengo')
+times.add("Vasco")
+times.add("Palmeiras")
+times.add("Fluminense")
+times.add("São Paulo")
+
+
+console.log(times)
+console.log(times.size)
