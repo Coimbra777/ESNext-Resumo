266bd0c492a0061af815bee476306f7f9a1f86c2
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-01-16 18:39:53 -0300

    estudos sobre node

---
+++ ESNext/promise2.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+// function primeiraLetra(string){
+//   return string[0]
+// }
+
+// function primeiroNome(string){
+//   return string[0]
+// }
+
+// const letraMinuscula = letra => letra.toLowerCase()
+
+// new Promise(function(cumprirPromessa){
+//   cumprirPromessa(['Ana', 'Bia', 'Carlos', 'Daniel'])
+// }).then(primeiraLetra)
+// .then(primeiroNome)
+// .then(console.log)
